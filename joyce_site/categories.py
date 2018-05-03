de47af73fc7ced65cf4549e567abc9f8a1f2e193
Lev Israel <eliezer.israel@gmail.com>
2018-05-03 09:34:38 -0400

    Now, with more ground

---
+++ joyce_site/categories.py
@@ -0,0 +1,21 @@
+
+# Giant list ordering or categories
+# indentation and inclusion of duplicate categories (like "Seder Moed")
+# is for readability only. The table of contents will follow this structure.
+CATEGORY_ORDER = [
+    "Joyces Head",
+    "Tail of Joyce",
+    "Relevant",
+    "Etc"
+]
+
+TOP_CATEGORIES = [
+    "Joyces Head",
+    "Tail of Joyce",
+    "Relevant",
+    "Etc"
+]
+
+REVERSE_ORDER = [
+]
+
